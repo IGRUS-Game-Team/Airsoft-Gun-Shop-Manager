ac8e505978eada195cb151d1f90bc0b760764c21
jiou22 <jiyeon08409@gmail.com>
2025-08-06 12:24:31 +0900

    [Feature#92-1] pont fix
    브랜치 새로 팔 때 났던 오류들 수정하고, 폰트 없어진 거 찾아서 수정

--- Assets/Src/Framework/UI/Adjustment/OnDayEnd.cs
+++ Assets/Src/Framework/UI/Adjustment/OnDayEnd.cs
@@ -22,9 +22,15 @@ public class OnDayEnd : MonoBehaviour
 
     private bool isAdjustmentCanvasActive = false; // 시간이 지났을 때만 정산 UI가 활성화되어야 하므로 false가 기본값
 
+
+    void Start()
+    {
+        GameObject.Find("hourAndMinute").GetComponent<TimeUI>();
+    }
+
     void Update()
     {
-        if (playerInput.dayEnd && !isAdjustmentCanvasActive) // enter 인식 && 
+        if (playerInput.dayEnd && !isAdjustmentCanvasActive) // enter 인식 && 정산캔버스활성화X
         {
             OnEnterDayEnd(); // EnterDayEnd라는 액션 인식 함수 호출
         }
@@ -33,7 +39,6 @@ public class OnDayEnd : MonoBehaviour
     void OnEnterDayEnd()
     {
         audioSource.PlayOneShot(AdjustmentAppearSound);
-
 
         AdjustmentCanvas.SetActive(true); 
         BackgroundImage.SetActive(true); 
