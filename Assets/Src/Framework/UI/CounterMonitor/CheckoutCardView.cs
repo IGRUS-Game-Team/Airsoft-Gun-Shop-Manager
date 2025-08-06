9b6592f757f699ee2383bccbd675498d2625436e
jeongminb933 <65400179+jeongminb933@users.noreply.github.com>
2025-08-06 17:42:59 +0900

    [feature] #151 #152

--- Assets/Src/Framework/UI/CounterMonitor/CheckoutCardView.cs
+++ Assets/Src/Framework/UI/CounterMonitor/CheckoutCardView.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 public class CheckoutCardView : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI nameText;
-    [SerializeField] TextMeshProUGUI amountText;
-    [SerializeField] TextMeshProUGUI totalPriceText;
+    [SerializeField] TextMeshProUGUI unitText;
+    [SerializeField] TextMeshProUGUI priceText;
+    [SerializeField] TextMeshProUGUI totalText;
 
-    public void Setup(CartSaveData item)
+    public void Setup(ItemData itemData, int amount) // 지금은 price가 정적으로 저장된 데이터인데
+                                                    // 나중에 상품 값 변경하는 기능 만들면 그거 뽑아와서 바꾸세요
     {
-        nameText.text       = item.itemName;
-        amountText.text     = $"x{item.amount}";
-        totalPriceText.text = $"${item.unitPrice * item.amount:F2}";
+        nameText.text = itemData.displayName;
+        unitText.text = amount.ToString();
+        priceText.text = $"${itemData.baseCost:F2}";
+        totalText.text = $"${itemData.baseCost * amount:F2}";
     }
 }
--- Assets/Src/Framework/UI/CounterMonitor/CountorMonitorController.cs
+++ Assets/Src/Framework/UI/CounterMonitor/CountorMonitorController.cs
@@ -1,16 +1,35 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CountorMonitorController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] Transform contentRoot;           // ScrollView의 Content 오브젝트
+    [SerializeField] CheckoutCardView cardPrefab;      // 위 프리팹
+
+    [SerializeField] TextMeshProUGUI totalPriceText;
+
+    public void Show(List<CounterSlotData> items)
     {
-        
+        Clear();
+
+        float total = 0f;
+
+        foreach (var slot in items)
+        {
+            var card = Instantiate(cardPrefab, contentRoot);
+            card.Setup(slot.itemData, slot.amount);
+            total += slot.itemData.baseCost * slot.amount;
+        }
+
+        totalPriceText.text = $"${total:F2}";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Clear()
     {
-        
+        foreach (Transform child in contentRoot)
+            Destroy(child.gameObject);
+
+        totalPriceText.text = "$0.00";
     }
 }
