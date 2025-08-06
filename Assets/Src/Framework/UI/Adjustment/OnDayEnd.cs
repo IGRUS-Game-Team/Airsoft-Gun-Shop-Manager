0df00beccc105e660c088a15e3ffc0e2107c54fd
jiou22 <jiyeon08409@gmail.com>
2025-08-06 18:39:41 +0900

    [Feature#43] Add AudioFiles and Fix featrue#92-1
    나중에 쓸 오디오파일들 추가
    
    feature#92-1에서 수정한 것들 전부 반영되지 않아 다시 수정하고 프리팹 적용

--- Assets/Src/Framework/UI/Adjustment/OnDayEnd.cs
+++ Assets/Src/Framework/UI/Adjustment/OnDayEnd.cs
@@ -19,7 +19,8 @@ public class OnDayEnd : MonoBehaviour
     [SerializeField] AudioClip UIAppearSound;
     [SerializeField] AudioSource audioSource;
     [SerializeField] StarterAssets.StarterAssetsInputs playerInput; // 액션맵
-    [SerializeField] TimeUI timeUI; 
+    [SerializeField] TimeUI timeUI;
+
 
     private bool isAdjustmentCanvasActive = false; // 시간이 지났을 때만 정산 UI가 활성화되어야 하므로 false가 기본값
 
@@ -30,6 +31,7 @@ public class OnDayEnd : MonoBehaviour
         bool isBetweenDeadline = hours >= 20; // 20:00 ~ 23:59 (마감시간)인지 
 
         if (playerInput.dayEnd && !isAdjustmentCanvasActive && isBetweenDeadline) // enter 인식 && 정산캔버스활성화X && 마감시간
+
         {
             OnEnterDayEnd(); // EnterDayEnd라는 액션 인식 함수 호출
         }
