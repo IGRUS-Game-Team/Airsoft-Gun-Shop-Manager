e7fea9cd9df34ef678dedc0108ac06852d46bf6a
jiou22 <jiyeon08409@gmail.com>
2025-08-06 14:40:00 +0900

    [Feature#92-1] DayEnd Time fix
    1. TimemUI 스크립트 내 totalGameMinutes에 public 추가
    2. OnDayEnd 스크립트에 totalGameMinutes 불러와서 게임 내 시간 20:00 ~ 23:59 사이에만 DayEnd 액션 활성화될 수 있도록 수정

--- Assets/Src/Framework/UI/Adjustment/OnDayEnd.cs
+++ Assets/Src/Framework/UI/Adjustment/OnDayEnd.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 using StarterAssets;
 
 /// <summary>
@@ -19,18 +18,17 @@ public class OnDayEnd : MonoBehaviour
     [SerializeField] AudioClip UIAppearSound;
     [SerializeField] AudioSource audioSource;
     [SerializeField] StarterAssets.StarterAssetsInputs playerInput; // 액션맵
+    [SerializeField] TimeUI timeUI; 
 
     private bool isAdjustmentCanvasActive = false; // 시간이 지났을 때만 정산 UI가 활성화되어야 하므로 false가 기본값
 
 
-    void Start()
-    {
-        GameObject.Find("hourAndMinute").GetComponent<TimeUI>();
-    }
-
     void Update()
     {
-        if (playerInput.dayEnd && !isAdjustmentCanvasActive) // enter 인식 && 정산캔버스활성화X
+        int hours = (timeUI.totalGameMinutes / 60) % 24; // 시간 계산 (TimeUI)
+        bool isBetweenDeadline = hours >= 20; // 20:00 ~ 23:59 (마감시간)인지 
+
+        if (playerInput.dayEnd && !isAdjustmentCanvasActive && isBetweenDeadline) // enter 인식 && 정산캔버스활성화X && 마감시간
         {
             OnEnterDayEnd(); // EnterDayEnd라는 액션 인식 함수 호출
         }
