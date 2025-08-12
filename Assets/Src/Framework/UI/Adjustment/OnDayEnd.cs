b771626e282fb5b3117930b1a42cd34065839506
jeongminb933 <65400179+jeongminb933@users.noreply.github.com>
2025-08-12 23:25:21 +0900

    수정

--- Assets/Src/Framework/UI/Adjustment/OnDayEnd.cs
+++ Assets/Src/Framework/UI/Adjustment/OnDayEnd.cs
@@ -18,31 +18,64 @@ public class OnDayEnd : MonoBehaviour
     [SerializeField] AudioClip AdjustmentAppearSound;
     [SerializeField] AudioClip UIAppearSound;
     [SerializeField] AudioSource audioSource;
-    [SerializeField] StarterAssets.StarterAssetsInputs playerInput; // 액션맵
+    //[SerializeField] StarterAssets.StarterAssetsInputs playerInput; // 액션맵
     [SerializeField] TimeUI timeUI;
 
-
     private bool isAdjustmentCanvasActive = false; // 시간이 지났을 때만 정산 UI가 활성화되어야 하므로 false가 기본값
+    private bool isEnterPressed = false;
+    public static bool isDayEndUIActive = false;
+    private void Start()
+    {
+        InteractionController.Instance.OnDayEnd += HandleExitKeyPressed;
+    }
+
+    private void OnDisable()
+    {
+        if (InteractionController.Instance != null)
+            InteractionController.Instance.OnDayEnd -= HandleExitKeyPressed;
+    }
+
+    private void HandleExitKeyPressed()
+    {
+        isEnterPressed = true;
+    }
+
+    private void OpenSetting()
+    {
+        isDayEndUIActive = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    private void CloseSetting() //정산 ui 꺼질때 이 메서드 넣어주세요
+    {
+        isDayEndUIActive = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false; //커서 안보임
+    }
 
 
     void Update()
     {
+        Debug.Log(isEnterPressed);
         int hours = (timeUI.totalGameMinutes / 60) % 24; // 시간 계산 (TimeUI)
-        bool isBetweenDeadline = hours >= 20; // 20:00 ~ 23:59 (마감시간)인지 
+        bool isBetweenDeadline = hours >= 20; // 20:00 ~ 23:59 (마감시간)인지
 
-        if (playerInput.dayEnd && !isAdjustmentCanvasActive && isBetweenDeadline) // enter 인식 && 정산캔버스활성화X && 마감시간
-
+        if (isEnterPressed && !isAdjustmentCanvasActive && isBetweenDeadline) // enter 인식 && 정산캔버스활성화X && 마감시간
         {
             OnEnterDayEnd(); // EnterDayEnd라는 액션 인식 함수 호출
+            isEnterPressed = false;
         }
-        else if (playerInput.dayEnd && !isAdjustmentCanvasActive && !isBetweenDeadline)
+        else if (isEnterPressed && !isAdjustmentCanvasActive && !isBetweenDeadline)
         {
+            isEnterPressed = false;
             Debug.Log("아직 마감 시간이 되지 않았습니다.");
         }
     }
 
     void OnEnterDayEnd()
     {
+        OpenSetting();
         audioSource.PlayOneShot(AdjustmentAppearSound);
 
         AdjustmentCanvas.SetActive(true); 
